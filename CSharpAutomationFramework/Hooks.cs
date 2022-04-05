7f887fe9e0f6cdc10f470d467a9e9cd8e0fd9b9b
Tcrosby23 <thomas.crosby@qualitestgroup.com>
2022-04-05 11:10:05 +0100

    initial commit

---
+++ CSharpAutomationFramework/Hooks.cs
@@ -0,0 +1,108 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using AventStack.ExtentReports.Reporter;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Reflection;
+
+namespace CSharpAutomationFramework
+{
+    [Binding]
+    class Hook
+    {
+        //TODO: make it system aware
+        const string outputFolder = "C:/Users/thomas.crosby/Documents/Projects/C#/CSharpAutomationFramework/CSharpAutomationFramework/Reports/";
+
+        private static ExtentTest? featureName;
+        private static ExtentTest? scenario;
+        private static ExtentReports? extent;
+
+        private DriverHelper _driverHelper;
+
+        public Hook(DriverHelper driverHelper)
+        {
+            _driverHelper = driverHelper;
+        }
+
+        [BeforeTestRun]
+        public static void InitializeReport()
+        {
+            var htmlReporter = new ExtentHtmlReporter(outputFolder);
+            htmlReporter.Config.Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
+
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+        }
+        [AfterTestRun]
+        public static void TearDownReport()
+        {
+            extent.Flush();
+        }
+        [AfterStep]
+        public void InsertReportingSteps(ScenarioContext sc)
+        {
+            var stepType = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString();
+            PropertyInfo pInfo = typeof(ScenarioContext).GetProperty("ScenarioExecutionStatus", BindingFlags.Instance | BindingFlags.Public);
+            MethodInfo getter = pInfo.GetGetMethod(nonPublic: true);
+            object TestResult = getter.Invoke(sc, null);
+            if (sc.TestError == null)
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "And")
+                    scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text);
+            }
+            if (sc.TestError != null)
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Fail(sc.TestError.Message);
+                if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Fail(sc.TestError.Message);
+                if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Fail(sc.TestError.Message);
+                if (stepType == "And")
+                    scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text).Fail(sc.TestError.Message);
+            }
+                        //Pending Status
+            if (TestResult.ToString() == "StepDefinitionPending")
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
+
+            }
+        }
+        [BeforeFeature]
+        public static void BeforeFeature(FeatureContext featurecontext)
+        {
+            featureName = extent.CreateTest(featurecontext.FeatureInfo.Title);
+        }
+
+
+        [BeforeScenario]
+        public void Initialize()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("-headless");
+            _driverHelper.webDriver = new ChromeDriver("C:/Users/thomas.crosby/Documents/Projects/Browser Drivers", options);
+            
+
+            //Create dynamic scenario name
+            scenario = featureName.CreateNode<Scenario>(ScenarioContext.Current.ScenarioInfo.Title);
+        }
+
+        [AfterScenario]
+        public void TearDown()
+        {
+            _driverHelper.webDriver.Dispose();
+
+        }
+    }
+}
---
+++ CSharpAutomationFramework/Pages/NFTPage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutomationFramework.Pages
+{
+    internal class NFTPage
+    {
+        private readonly IWebDriver driver;
+        //private IWebElement searchBar => driver.FindElement(By.CssSelector("#sbcId > form > input"));
+        private IWebElement googleBar => driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+        private IWebElement cookiesButton => driver.FindElement(By.Id("L2AGLb"));
+        public NFTPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void EnterSearchTerm(string searchTerm)
+        {
+            googleBar.SendKeys(searchTerm);
+        }
+
+        public void AcceptCookies()
+        {
+            cookiesButton.Click();
+        }
+
+        public string ReturnURL()
+        {
+            return driver.Url;
+        }
+    }
+}
