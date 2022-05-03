8346a7af87b129bcca010c69b399cc019af2a0b1
leigh966 <leigh966@hotmail.com>
2022-05-03 15:08:47 +0100

    Added self testing folder to project for developing unit tests for methods that are difficult to test effectively in specflow. Within this folder, a class for testing genericReusableComponents has been made with 8 test cases for the generateLocator method. To do this easily, generateLocator has been made static as there is no reason to not have it static in its current context

--- CSharpAutomationFramework/reusable/GenericReusableComponents.cs
+++ CSharpAutomationFramework/reusable/GenericReusableComponents.cs
@@ -18,7 +18,7 @@ namespace CSharpAutomationFramework.reusable
         /// <param name="locatorValue">Type of locator ("XPATH", "ID", "NAME" or "LINKTEXT)</param> 
         /// <param name="locatorType">Value of the locator</param>
         /// TODO: IMPLEMENT LOGGING FOR DEFAULT CASE
-        public By generateLocator(string locatorType, string locatorValue)
+        public static By generateLocator(string locatorType, string locatorValue)
         {
             By loc = null;
             switch(locatorType)
