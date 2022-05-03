172fcd0fba52de0612dbc5b0534b98898c4ab015
leigh966 <leigh966@hotmail.com>
2022-05-03 15:59:30 +0100

    Moved reusables feature file to selfTest and created basic starter code for WebReusableComponentsTests

---
+++ CSharpAutomationFramework/SelfTesting/WebReusableComponentsTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutomationFramework.SelfTesting.reusable
+{
+    public class WebReusableComponentsTests
+    {
+        public IWebDriver driver = new ChromeDriver(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\Drivers\\");
+        class WRC_Tester : CSharpAutomationFramework.reusable.WebReusableComponents
+        {
+            public WRC_Tester(IWebDriver driver) : base(driver, ("https://google.co.uk",""))
+            {
+
+            }
+        }
+        WRC_Tester tester;
+        public void BeforeEach()
+        {
+            tester = new WRC_Tester(driver);
+        }
+
+        [Test]
+        public void testTest()
+        {
+            BeforeEach();
+            tester.navigateHome();
+        }
+
+    }
+}
